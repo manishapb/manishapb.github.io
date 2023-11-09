d771d3e6e04da1f552cfac3f829f6a94de9860b0
Manisha Pillai <pillaimanisha77@gmail.com>
2023-11-10 03:16:36 +0530

    add a basic intro

--- src/App.js
+++ src/App.js
@@ -5,19 +5,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Mansiha Pillai
       </header>
+      <><p>
+        <a href='https://github.com/manishapb'>Github</a>
+        <a href='https://www.linkedin.com/in/manisha-pillai/'>Linkedin</a>
+        <a href=''>CV</a>
+      </p>
+      </>
+      <div>
+        <p>I am Software Engineer by profession and an artst at heart.</p>
+        <p>Some languges I've loved and currently use are:</p>
+        <ul>
+          <li>Python</li>
+          <li>ReactJS</li>
+          <li>Clojure</li>
+          </ul>
+        <p>Previously worked in : Php, JS, NodeJS</p>
+        <p>I'm currently looking for some interesting work. I have previously at 
+          <a href='https://www.gobolt.in/'>GoBOLT</a> and 
+          <a href='https://www.knowlarity.com/'></a>Knowlarity</p>
+        <p>I have Master's in Computer Application from University of Pune which I wrapped up with a couple of
+           internships at <a href='https://www.globant.com/'></a>Globant and 
+           <a href='https://www.helpshift.com/'></a>Helpshift</p>
+      </div>
+      <footer>
+      <p class="love">Made with <i class="icon ion-heart"></i> in React</p>
+      </footer>
     </div>
   );
 }
