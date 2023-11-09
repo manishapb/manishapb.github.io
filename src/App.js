4d630f2cd1530d9895d5277ef63c2d56ca5bec4a
Manisha Pillai <pillaimanisha77@gmail.com>
2023-11-10 03:49:31 +0530

    update a basic intro

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          Mansiha Pillai
+          Manisha Pillai
       </header>
       
       <div>
