570dff475663f3ad5a3aa7b7eea50690f9994721
Manisha Pillai <pillaimanisha77@gmail.com>
2023-11-12 04:07:05 +0530

    replace img with svg and update css

--- src/App.js
+++ src/App.js
@@ -8,29 +8,29 @@ function App() {
   const linkIcon = [{
     'name': 'Github',
     'link': 'https://github.com/manishapb/',
-    'icon': 'github.png'
+    'icon': 'github.svg'
   },
   {
     'name': 'Linkedin',
     'link': 'https://www.linkedin.com/in/manisha-pillai',
-    'icon': 'linkedin.png'
+    'icon': 'linkedin.svg'
   },
   {
     'name': 'CV',
     'link': 'Manisha Pillai - Resume.pdf',
-    'icon': 'cv.jpeg'
+    'icon': 'file-pdf.svg'
   },
   {
     'name': 'mail',
-    'link': 'pillaimanisha77@gmail.com',
-    'icon': 'mail.png'
+    'link': 'mailto:pillaimanisha77@gmail.com',
+    'icon': 'envelope.svg'
   }
-];
-  const links = linkIcon.map((i)=> <a href={i.link}>
-                  <img src={i.icon} className='' alt={i.name}></img></a>)
+  ];
+  const links = linkIcon.map((i) => <a href={i.link}>
+    <img src={i.icon} className='img-icon' alt={i.name}></img></a>)
   return (
     <>
-      <div className='App container-fluid text-secondary'>
+      <div className='App container-fluid text-dark'>
         <div className="container fs-5">
           <div className="row">
             <div className="col-9">
@@ -40,7 +40,7 @@ function App() {
             </div>
             <div className="col-4">
               <br></br>
-              <img src='profilepic.png' className='rounded float-start'
+              <img src='profilepic.png' className='rounded float-start profile-img'
                 alt='profile pic'></img>
             </div>
             <div className="col-6">
@@ -50,13 +50,13 @@ function App() {
                   <br></br>
                   Welcome to my webpage!
                   <br></br>
-                  I am Software Engineer exploring the world and life as a digital nomad.
+                  I am Software Engineer. Exploring the world and life as a digital nomad.
                 </p>
-                <div className='navbar'>
+                <div className='navbar ms-4 me-2'>
                   {links}
                 </div>
                 <p>
-                  {}
+                  { }
                 </p>
               </div>
             </div>
@@ -65,28 +65,26 @@ function App() {
 
         <div className='container fs-5'>
           <br></br>
-          <p>Languages I've loved working with:
+          <p>Languages I've been working with: 
             {(langs).join(', ')}</p>
           <div className='container'>
             <br></br>
             <p>I have previously worked at
-              <a href='https://www.gobolt.in/'>GoBOLT</a> and
-              <a href='https://www.knowlarity.com/'>Knowlarity</a> and
-              <a href='https://www.graas.ai/'>Shoptimize</a></p>
+              <a href='https://www.gobolt.in/' className='text-muted ms-1'>GoBOLT</a> and
+              <a href='https://www.knowlarity.com/' className='text-muted ms-1'>Knowlarity</a> and
+              <a href='https://www.graas.ai/' className='text-muted ms-1'>Shoptimize</a></p>
             <p>I have Master's in Computer Application from
               <p></p>University of Pune which
               I wrapped up with a couple of
-              internships at <a href='https://www.globant.com/'>Globant</a> and
-              <a href='https://www.helpshift.com/'>Helpshift</a></p>
+              internships at <a href='https://www.globant.com/' className='text-muted ms-1'>Globant</a> and
+              <a href='https://www.helpshift.com/' className='text-muted ms-1'>Helpshift</a></p>
           </div>
         </div>
         <footer>
           <br></br>
           <div className='fs-8 mt-6'></div>
-          <p className=''>Made with <i className="bi bi-heart">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-              <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-            </svg>
+          <p className='text-muted'>Made with <i className="bi bi-heart">
+          <img src='heart.svg' className='img'></img>
           </i> in React</p>
         </footer>
       </div>
